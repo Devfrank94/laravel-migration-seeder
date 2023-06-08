94a36d47dcf56103cef09d39381aac6d500f0454
Devfrank94 <francescomurro94@gmail.com>
2023-06-08 18:52:23 +0200

    create seeder

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -15,6 +15,19 @@ class TrainsTableSeeder extends Seeder
      * @return void
      */
     public function run(Faker $faker){
+        for($i = 0; $i < 30; $i++){
+        $new_train = new Train();
+        $new_train->agency = $faker->randomElement(['Agency1', 'Agency2', 'Agency3']);
+        $new_train->departure_station = $faker->city();
+        $new_train->arrival_station = $faker->city();
+        $new_train->departure_time = $faker->date() . ' '. $faker->time();
+        $new_train->arrival_time = $faker->date() . ' '. $faker->time();
+        $new_train->train_code = $faker->isbn10();
+        $new_train->number_of_carriages = rand(3,10);
+        $new_train->in_time = rand(0,1);
+        $new_train->deleted = rand(0,1);
+        // dump($new_train);
         
+        }
     }
 }
